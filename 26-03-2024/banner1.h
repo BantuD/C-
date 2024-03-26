e06856488c20aa759a8e4b106d183d31b848c36e
BantuD <rajuldubey30@gmail.com>
2024-03-26 11:36:43 +0530

    updated notes created testClassBanner.pp banner1.cpp

--- 26-03-2024/banner1.h
+++ 26-03-2024/banner1.h
@@ -10,21 +10,47 @@ class Banner
 		//parameters is called a default constructor and such 
 		//a constructor is automatically defined for a class 
 		//which does not explicityly define any constructor 
+		
+		
+		//* this is default constructor also
 		Banner()
 		{
 			width=20;
 			height=5;
 			triangular=false;
 		}
+		//Banner::Resize(Banner* this, float w,float h)
+		
+	void Resize(float w, float h)
+		{
+			width=w; //this[0].width = w;
+			height=h; // this[0].height = h;
+		}
+
+		void Triangulate(bool yes)
+		{
+			triangular = yes;
+		}
+		// a const method does not change the state of the
+		// object on which it is called
+		double Price()  const
+		{
+			float k=triangular ? 0.5 : 1.0;
+			float rate=width <= height?0.75:0.80;
+
+			return k*width*height*rate;
+		}
+
 	
+
 
 	private:   // will not be visible out side of the class
 		
-		float widht,height;  // now no one can interact with 
+		float width,height;  // now no one can interact with 
 				     // these because they are private
 				     // we hide them from the others
 		bool triangular;
 
 
 
-}
+};
---
+++ 26-03-2024/testClassBanner.cpp
@@ -0,0 +1,18 @@
+#include "banner1.h"
+#include<cstdio>
+
+int main(void)
+{
+	Banner a;
+	printf("Price of standard banner: %.2f\n",a.Price()); //binding happening
+							      //
+	
+	Banner b;
+	float w,h;
+	printf("Dimesions of custom banner: ");
+	scanf("%f%f",&w,&h);
+	b.Resize(w,h); // Banner: Resize(&b,w,h) // binding happening
+	printf("Price of custom rectangular banner: %.2f\n",b.Price());
+	b.Triangulate(true); // binding happening
+	printf("Price of custom triangular banner : %.2f\n",b.Price()); // binding happening 
+}
